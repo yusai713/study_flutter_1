8f1c8802d290a8298926e48b53349758c2682801
西原宏太 <nishihara@nishiharakoutanoMacBook-puro.local>
2020-12-26 00:26:55 +0900

    second commit

---
+++ android/app/src/main/kotlin/com/yusai/init_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yusai.init_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:init_project/next_page.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final items = List<String>.generate(10000, (i) => "Item $i");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //ヘッダー
+      appBar: AppBar(
+        title: Text("ニッチる"),
+        actions: <Widget>[
+          Icon(Icons.add),
+          Icon(Icons.share),
+          Icon(Icons.cached),
+        ],
+      ),
+
+      //ボディー
+      body: Container(
+       width: double.infinity,
+        child: ListView(
+          // This next line does the trick.
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            Container(
+              width: 160.0,
+              color: Colors.red,
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.green,
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.yellow,
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.orange,
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
